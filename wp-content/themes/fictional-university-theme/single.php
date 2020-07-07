714f6844d6770948db427e699cca9ae8f6a5a10c
Mark Tiddy <marktiddy@gmail.com>
2020-07-07 10:45:28 +0100

    Setting up basics and adding placeholder content

---
+++ wp-content/themes/fictional-university-theme/single.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+
+<?php 
+//This loops through all psots and is a WP func
+while(have_posts()) : 
+  //Call the post
+  the_post(); ?>
+<h1><?php the_title(); ?></h1>
+<p><?php the_content(); ?></p>
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
+
+
