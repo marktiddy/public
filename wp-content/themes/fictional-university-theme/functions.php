853caa12251c6c75a41578ba201755069439e558
Mark Tiddy <marktiddy@gmail.com>
2020-07-15 17:06:21 +0100

    Shortening code

--- wp-content/themes/fictional-university-theme/functions.php
+++ wp-content/themes/fictional-university-theme/functions.php
@@ -1,4 +1,34 @@
 <?php
+
+//Custom function for page banners
+function pageBanner($args = NULL) {
+if (!$args['title']) {
+$args['title'] = get_the_title();
+}
+if (!$args['subtitle']) {
+  $args['subtitle'] = get_field('page_banner_subtitle');
+}
+if (!$args['photo']) {
+  if (get_field('page_banner_background_image')) {
+    $args['photo'] = get_field('page_banner_background_image')['sizes']['pageBanner'];
+  } else {
+    $args['photo'] = get_theme_file_uri('/images/ocean.jpg');
+  }
+}
+?>
+<div class="page-banner">
+    <div class="page-banner__bg-image" style="background-image: url(<?php echo $args['photo']; ?>);"></div>
+    <div class="page-banner__content container container--narrow">
+      <h1 class="page-banner__title"><?php echo $args['title']; ?></h1>
+      <div class="page-banner__intro">
+        <p><?php echo $args['subtitle']; ?></p>
+      </div>
+    </div>  
+  </div>
+<?php
+}
+
+
 //Our function
 function university_files() {
   //Here we load out css and javascript
